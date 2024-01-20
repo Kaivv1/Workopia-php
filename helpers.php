3228f5d37c669715254b6c4e8b6306a4706df027
Kaloyan Vulchev <kvalchevv1@gmail.com>
2024-01-20 18:05:53 +0200

    added partials, basic route and controllers

--- helpers.php
+++ helpers.php
@@ -4,3 +4,41 @@ function basePath($path = '')
 {
   return __DIR__ . '/' . $path;
 }
+
+function loadView($name)
+{
+  $viewPath =  basePath("views/{$name}.view.php");
+
+  if (file_exists($viewPath)) {
+    require $viewPath;
+  } else {
+    echo "View {$name} not found!";
+  }
+}
+
+function loadPartial($name)
+{
+  $partialPath = basePath("views/partials/{$name}.php");
+
+  if (file_exists($partialPath)) {
+    require $partialPath;
+  } else {
+    echo "Partial {$name} was not found!";
+  }
+}
+
+
+
+function inspect($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
+
+function inspectAndDie($value)
+{
+  echo '<pre>';
+  die(var_dump($value));
+  echo '</pre>';
+}
