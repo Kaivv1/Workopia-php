ecd36e9e0ad02db1624812eab7529c69d18dbdb2
Kaloyan Vulchev <kvalchevv1@gmail.com>
2024-01-23 15:39:37 +0200

    added login and register feature

--- public/index.php
+++ public/index.php
@@ -1,12 +1,11 @@
 <?php
-session_start();
-
 require __DIR__ . '/../vendor/autoload.php';
+
+use Framework\Router, Framework\Session;
+
+Session::start();
+
 require '../helpers.php';
-
-use Framework\Router;
-
-
 $router = new Router();
 $routes = require basePath('routes.php');
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
