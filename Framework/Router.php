730b6add4d1c60cd3d50a64a9afed16ee29babff
Kaloyan Vulchev <kvalchevv1@gmail.com>
2024-02-27 19:34:05 +0200

    Update Router.php

--- Framework/Router.php
+++ Framework/Router.php
@@ -42,10 +42,7 @@ class Router
   {
     $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-    // Check for _method input
     if ($requestMethod === 'POST' && isset($_POST['_method'])) {
-      // override the request method with the value of _method
-
       $requestMethod = strtoupper($_POST['_method']);
     }
 
