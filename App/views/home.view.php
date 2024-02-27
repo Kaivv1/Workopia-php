5b064eaa06e3028c2ccc7843c328ad6d357e382b
Kaloyan Vulchev <kvalchevv1@gmail.com>
2024-02-27 19:36:30 +0200

    Update home.view.php

--- App/views/home.view.php
+++ App/views/home.view.php
@@ -6,7 +6,6 @@
 
 <?php loadPartial('top-banner') ?>
 
-<!-- Job Listings -->
 <section>
   <div class="container mx-auto p-4 mt-4">
     <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Recent Jobs</div>
